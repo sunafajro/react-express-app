96c9aaf4460b4ac8758f3e9770986f240cd13bac
Evgeny Belkin sunafajro@gmail.com <sunafajro@gmail.com>
2017-07-24 21:06:49 +0300

    делаем лейауты в основном контейнере

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 //import { connect } from 'react-redux';
 //import { push } from 'react-router-redux';
-import { Menu, Icon } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 import './App.css'
 import Home from '../components/Home';
 import Admin from '../components/Admin';
 import Users from '../components/Users';
 import Login from '../components/Login';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
   constructor() {
@@ -25,29 +27,40 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Menu
-          onClick={this.handleMenuClick}
-          selectedKeys={[ this.state.currentMenuItem ]}
-          mode="horizontal"
-        >
-          <Menu.Item key="home">
-            <Icon type="home" /> <Link to="/" />
-          </Menu.Item>
-          <Menu.Item key="login">
-            <Icon type="login" /> <Link to="/login" />
-          </Menu.Item>
-          <Menu.Item key="Admin">
-            <Icon type="admin" /> <Link to="/admin" />
-          </Menu.Item>
-        </Menu>
-        <Switch>      
-          <Route exact path='/admin' component={ Admin } />
-          <Route exact path='/users' component={ Users } />
-          <Route exact path='/login' component={ Login } />
-          <Route exact path='/' component={ Home } />
-        </Switch>
-      </div>
+      <Layout>
+          <Header>
+          <Menu
+            theme="dark"
+            defaultSelectedKeys={['home']}
+            onClick={this.handleMenuClick}
+            selectedKeys={[ this.state.currentMenuItem ]}
+            mode="horizontal"
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="home">
+              <Icon type="home" /> <Link to="/" />
+            </Menu.Item>
+            <Menu.Item key="login">
+              <Icon type="login" /> <Link to="/login" />
+            </Menu.Item>
+            <Menu.Item key="Admin">
+              <Icon type="admin" /> <Link to="/admin" />
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Sider></Sider>
+          <Content>
+            <Switch>      
+              <Route exact path='/admin' component={ Admin } />
+              <Route exact path='/users' component={ Users } />
+              <Route exact path='/login' component={ Login } />
+              <Route exact path='/' component={ Home } />
+            </Switch>
+          </Content>
+        </Layout>
+        <Footer>Creted by Evgeny Belkin 2017</Footer>
+      </Layout>
     );
   }
 }
